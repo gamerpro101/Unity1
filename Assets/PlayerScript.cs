a8d171aff98437fad37032c92bc5fb73a6ef1fcf
gamerpro101 <scrambledhandy@gmail.com>
2023-08-22 23:03:35 -0400

    Can now go up and down.

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -8,6 +8,8 @@ public class PlayerScript : MonoBehaviour {
 
 	private float minX = -2.55f;
 	private float maxX = 2.55f;
+	private float minY = -4f;
+	private float maxY = 4f;
 
 	void Update () {
 		MovePlayer ();
@@ -16,6 +18,7 @@ public class PlayerScript : MonoBehaviour {
 	void MovePlayer() {
 
 		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis("Vertical");
 		Vector2 currentPosition = transform.position;
 
 		if (h > 0) {
@@ -28,6 +31,19 @@ public class PlayerScript : MonoBehaviour {
 
 			currentPosition.x -= speed * Time.deltaTime;
 		}
+		if (v > 0)
+		{
+			// going to the up  side
+
+			currentPosition.y += speed * Time.deltaTime;
+
+		}
+		else if (v < 0)
+		{
+			// going to the down side
+
+			currentPosition.y -= speed * Time.deltaTime;
+		}
 
 		if (currentPosition.x < minX) {
 			currentPosition.x = minX;
@@ -35,6 +51,16 @@ public class PlayerScript : MonoBehaviour {
 
 		if (currentPosition.x > maxX) {
 			currentPosition.x = maxX;
+		}
+
+		if (currentPosition.y < minY)
+		{
+			currentPosition.y = minY;
+		}
+
+		if (currentPosition.y > maxY)
+		{
+			currentPosition.y = maxY;
 		}
 
 		transform.position = currentPosition;
